765e4b7eb8246fef88ce4823769ee08db4ea98cc
Eric Tang <tangcongyuan@gmail.com>
2016-01-20 15:45:56 -0500

    Local code ready to merge. Nothing worth noting.

--- tangcongyuan_com/tangcongyuan_com/settings.py
+++ tangcongyuan_com/tangcongyuan_com/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,8 +77,14 @@ WSGI_APPLICATION = 'tangcongyuan_com.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'eric',
+        'USER': 'eric',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
